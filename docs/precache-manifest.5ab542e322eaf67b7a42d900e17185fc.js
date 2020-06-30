c2e65eea599429af065c7fe9a3bd3311a529063e
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2020-06-30 12:07:09 -0600

    [RELEASE]: First react-build to deploy it in github pages

---
+++ docs/precache-manifest.5ab542e322eaf67b7a42d900e17185fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7cd9918bc46cab198ccb0b85483086",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cce73ec0122c68b7eae",
+    "url": "/static/css/main.660b7d48.chunk.css"
+  },
+  {
+    "revision": "c01635c84c42dd6e6742",
+    "url": "/static/js/2.a6c06534.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a6c06534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cce73ec0122c68b7eae",
+    "url": "/static/js/main.9f1686b4.chunk.js"
+  },
+  {
+    "revision": "43914f2200b576e6b097",
+    "url": "/static/js/runtime-main.24c99475.js"
+  }
+]);
