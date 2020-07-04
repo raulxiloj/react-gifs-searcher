aa8ec0c74d4c578987f24e9f45a24139a29f84b6
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2020-07-03 23:04:39 -0600

    docs folder added for github pages

---
+++ docs/precache-manifest.8248b30a716ef943fc342780060d3980.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03ae5cdb89dca9697bae87f384804316",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4319f202b6055a621535",
+    "url": "/static/css/main.a0012c17.chunk.css"
+  },
+  {
+    "revision": "c01635c84c42dd6e6742",
+    "url": "/static/js/2.a6c06534.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a6c06534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4319f202b6055a621535",
+    "url": "/static/js/main.7b3da1b5.chunk.js"
+  },
+  {
+    "revision": "43914f2200b576e6b097",
+    "url": "/static/js/runtime-main.24c99475.js"
+  }
+]);
